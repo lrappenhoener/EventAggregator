3265632dbdbdd6d937a39a4253f3a8047a691885
lr <lrmail>
2022-03-30 15:26:40 +0200

    Add test "Subscribed_Handler_Invoked_When_Event_Published"

--- tests/UnitTests/Tests.cs
+++ tests/UnitTests/Tests.cs
@@ -1,6 +1,25 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
 namespace PCC.Libraries.EventAggregator.UnitTests;
 
 public abstract class Tests
 {
     protected abstract IEventAggregator CreateSut();
+
+    [Fact]
+    public void Subscribed_Handler_Invoked_When_Event_Published()
+    {
+        var sut = CreateSut();
+        var expectedEvent = new SampleEvent("TestMessage", 42);
+        var expectedSender = new object();
+        var invoked = false;
+        void Handler(object o, SampleEvent e) => invoked = e == expectedEvent && o == expectedSender;
+        sut.Subscribe<SampleEvent>(Handler);
+
+        sut.Publish(expectedSender, expectedEvent);
+
+        invoked.Should().BeTrue();
+    }
 }
