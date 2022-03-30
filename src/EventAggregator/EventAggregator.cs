deb85a43cb45bb42f10582042db1f63093def9c3
lr <lrmail>
2022-03-30 15:23:36 +0200

    Implement Subscribe

--- src/EventAggregator/EventAggregator.cs
+++ src/EventAggregator/EventAggregator.cs
@@ -2,9 +2,15 @@ namespace PCC.Libraries.EventAggregator;
 
 public class EventAggregator : IEventAggregator
 {
+    private readonly Dictionary<Type, List<object>> _handlers =
+        new Dictionary<Type, List<object>>();
+
     public void Subscribe<T>(EventHandler<T> handler)
     {
-        
+        var type = typeof(T);
+        if (!_handlers.ContainsKey(type))
+            _handlers.Add(type, new List<object>());
+        _handlers[type].Add(handler);
     }
 
     public void Publish<T>(object sender, T @event)
