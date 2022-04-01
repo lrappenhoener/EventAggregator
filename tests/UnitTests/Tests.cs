335841d71018879e6b9abb34ab025d618dbc5520
lr <lrmail>
2022-04-01 15:38:44 +0200

    Split Subscribe and Unsubscribe tests between generic and non generic

--- tests/UnitTests/Tests.cs
+++ tests/UnitTests/Tests.cs
@@ -10,7 +10,7 @@ public abstract class Tests
     protected abstract IEventAggregator CreateSut();
 
     [Fact]
-    public void Subscribed_Handler_Invoked_When_Event_Published()
+    public void Subscribe_Of_T_Handler_Invoked_When_Event_Published()
     {
         var sut = CreateSut();
         var expectedEvent = new SampleEvent("TestMessage", 42);
@@ -25,7 +25,22 @@ public abstract class Tests
     }
     
     [Fact]
-    public void Subscribed_Handlers_All_Invoked_When_Event_Published()
+    public void Subscribe_Handler_Invoked_When_Event_Published()
+    {
+        var sut = CreateSut();
+        var expectedEvent = new SampleEvent("TestMessage", 42);
+        var expectedSender = new object();
+        var invoked = false;
+        void Handler(object o, SampleEvent e) => invoked = e == expectedEvent && o == expectedSender;
+        sut.Subscribe(typeof(SampleEvent), Handler);
+
+        sut.Publish(expectedSender, expectedEvent);
+
+        invoked.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Subscribe_Of_T_Handlers_All_Invoked_When_Event_Published()
     {
         var sut = CreateSut();
         var expectedEvent = new SampleEvent("TestMessage", 42);
@@ -44,7 +59,26 @@ public abstract class Tests
     }
     
     [Fact]
-    public void Unsubscribed_Handler_Not_Invoked_When_Event_Published()
+    public void Subscribe_Handlers_All_Invoked_When_Event_Published()
+    {
+        var sut = CreateSut();
+        var expectedEvent = new SampleEvent("TestMessage", 42);
+        var expectedSender = new object();
+        var invocations = new bool[3];
+        void Handler(object o, SampleEvent e) => invocations[0] = e == expectedEvent && o == expectedSender;
+        void Handler2(object o, SampleEvent e) => invocations[1] = e == expectedEvent && o == expectedSender;
+        void Handler3(object o, SampleEvent e) => invocations[2] = e == expectedEvent && o == expectedSender;
+        sut.Subscribe(typeof(SampleEvent), Handler);
+        sut.Subscribe(typeof(SampleEvent), Handler2);
+        sut.Subscribe(typeof(SampleEvent), Handler3);
+
+        sut.Publish(expectedSender, expectedEvent);
+
+        invocations.All(invoked => invoked).Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Unsubscribe_Of_T_Handler_Not_Invoked_When_Event_Published()
     {
         var sut = CreateSut();
         var expectedEvent = new SampleEvent("TestMessage", 42);
@@ -60,11 +94,36 @@ public abstract class Tests
     }
     
     [Fact]
-    public void Unsubscribed_Not_Subscribed_Handler_Not_Throws()
+    public void Unsubscribe_Handler_Not_Invoked_When_Event_Published()
+    {
+        var sut = CreateSut();
+        var expectedEvent = new SampleEvent("TestMessage", 42);
+        var expectedSender = new object();
+        var invoked = false;
+        void Handler(object o, SampleEvent e) => invoked = e == expectedEvent && o == expectedSender;
+        sut.Subscribe(typeof(SampleEvent), Handler);
+        sut.Unsubscribe(typeof(SampleEvent), Handler);
+
+        sut.Publish(expectedSender, expectedEvent);
+
+        invoked.Should().BeFalse();
+    }
+    
+    [Fact]
+    public void Unsubscribe_Of_T_With_Not_Subscribed_Handler_Not_Throws()
     {
         var sut = CreateSut();
         void Handler(object o, SampleEvent e) { }
 
         Record.Exception(() => sut.Unsubscribe<SampleEvent>(Handler)).Should().BeNull();
+    }
+    
+    [Fact]
+    public void Unsubscribe_With_Not_Subscribed_Handler_Not_Throws()
+    {
+        var sut = CreateSut();
+        void Handler(object o, SampleEvent e) { }
+
+        Record.Exception(() => sut.Unsubscribe(typeof(SampleEvent), Handler)).Should().BeNull();
     }
 }
